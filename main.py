1a3b96962bbe673add434afd829450e20416c95f
chenhao-123-sudo <64683411+chenhao-123-sudo@users.noreply.github.com>
2024-08-05 09:33:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
 from ultralytics import YOLO
 
-model = YOLO("ultralytics/models/v8/yolov8_CSAT.yaml").load("./yolov8m.pt")
+model = YOLO("ultralytics/models/v8/MFRENet.yaml").load("./yolov8m.pt")
 model.train(data="ultralytics/datasets/VisDrone_406.yaml",
             epochs=150, batch=2, imgsz=800, lr0=0.001,patience=50)

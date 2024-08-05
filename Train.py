cba2ecc1b528754ee4cf632593d2401fcfa8311d
chenhao-123-sudo <64683411+chenhao-123-sudo@users.noreply.github.com>
2024-08-05 09:33:41 +0800

    Update Train.py

--- Train.py
+++ Train.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
 from ultralytics import YOLO
 
-model = YOLO("ultralytics/models/v8/yolov8.yaml").load("./yolov8n.pt")
-model.train(data="/home/user/chenhao/ultralytics/ultralytics/datasets/VisDrone_406.yaml",
-            epochs=500, batch=2, imgsz=800, lr0=0.001,patience=50)
+model = YOLO("ultralytics/models/v8/MFRENet.yaml").load("./yolov8m.pt")
+model.train(data="ultralytics/datasets/VisDrone_406.yaml",
+            epochs=150, batch=2, imgsz=800, lr0=0.001,patience=50)
 
